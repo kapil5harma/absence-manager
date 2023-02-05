c147c0250ae80c98020874c4a8cf311a455cf665
Kapil Sharma <kapil.sharma.se@gmail.com>
2023-02-06 00:39:16 +0530

    refactor: prettier changes for api routes

--- pages/api/absence.ts
+++ pages/api/absence.ts
@@ -3,28 +3,14 @@ import { promises as fs } from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
 import mergeArrays from '@/shared/mergeArrays';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const jsonDirectory = path.join(process.cwd(), 'json');
-    const memberContents = await fs.readFile(
-      jsonDirectory + '/members.json',
-      'utf8'
-    );
-    const absenceContents = await fs.readFile(
-      jsonDirectory + '/absences.json',
-      'utf8'
-    );
-    const mergedContent = mergeArrays(
-      JSON.parse(absenceContents)?.payload,
-      JSON.parse(memberContents)?.payload
-    );
+    const memberContents = await fs.readFile(jsonDirectory + '/members.json', 'utf8');
+    const absenceContents = await fs.readFile(jsonDirectory + '/absences.json', 'utf8');
+    const mergedContent = mergeArrays(JSON.parse(absenceContents)?.payload, JSON.parse(memberContents)?.payload);
     res.status(200).json(mergedContent);
   } catch (err) {
-    res
-      .status(500)
-      .json({ message: 'Something went wrong trying to get data' });
+    res.status(500).json({ message: 'Something went wrong trying to get data' });
   }
 }
--- pages/api/member.ts
+++ pages/api/member.ts
@@ -2,20 +2,12 @@ import path from 'path';
 import { promises as fs } from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const jsonDirectory = path.join(process.cwd(), 'json');
-    const fileContents = await fs.readFile(
-      jsonDirectory + '/members.json',
-      'utf8'
-    );
-    res.status(200).json(JSON.parse(fileContents));
+    const fileContents = await fs.readFile(jsonDirectory + '/members.json', 'utf8');
+    res.status(200).json(JSON.parse(fileContents).payload);
   } catch (err) {
-    res
-      .status(500)
-      .json({ message: 'Something went wrong trying to get data' });
+    res.status(500).json({ message: 'Something went wrong trying to get data' });
   }
 }
