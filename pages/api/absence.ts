e9edccdf7a22f13623a44fde3b725574609107f1
Kapil Sharma <kapil.sharma.se@gmail.com>
2023-02-05 12:43:56 +0530

    refactor: error handling and modify absence response

--- pages/api/absence.ts
+++ pages/api/absence.ts
@@ -1,15 +1,30 @@
-import path from 'path'
-import { promises as fs } from 'fs'
-import { NextApiRequest, NextApiResponse } from 'next'
+import path from 'path';
+import { promises as fs } from 'fs';
+import { NextApiRequest, NextApiResponse } from 'next';
+import mergeArrays from '@/shared/mergeArrays';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const jsonDirectory = path.join(process.cwd(), 'json')
-  const fileContents = await fs.readFile(
-    jsonDirectory + '/absences.json',
-    'utf8'
-  )
-  res.status(200).json(JSON.parse(fileContents))
+  try {
+    const jsonDirectory = path.join(process.cwd(), 'json');
+    const memberContents = await fs.readFile(
+      jsonDirectory + '/members.json',
+      'utf8'
+    );
+    const absenceContents = await fs.readFile(
+      jsonDirectory + '/absences.json',
+      'utf8'
+    );
+    const mergedContent = mergeArrays(
+      JSON.parse(absenceContents)?.payload,
+      JSON.parse(memberContents)?.payload
+    );
+    res.status(200).json(mergedContent);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: 'Something went wrong trying to get data' });
+  }
 }
